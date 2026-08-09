a6c3168c5df63d4af9a951f166ece9283f979328
Mdshaikh08 <shaikhadnananis@gmail.com>
2026-08-10 00:44:37 +0530

    Compilation Pipeline

---
+++ Phase-1-C-fundamentals/13_Multi_Files_Compilation_Pipelines/main.c
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+int main(){
+    printf("%d \n" , counter);
+
+    increment_counter();
+    increment_counter();
+    increment_counter();
+
+    printf("%d \n" , counter);
+    
+}
+
---
+++ Phase-1-C-fundamentals/13_Multi_Files_Compilation_Pipelines/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include<stdio.h>
+#include<stdint.h>
+
+extern int counter ;
+
+void increment_counter(void);
+
+
+#endif
